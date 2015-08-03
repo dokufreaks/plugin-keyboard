40ca8928b3c349947a4ba05a3056040da56f7b84
LarsDW223 <lars_paulsen@web.de>
2015-08-03 20:30:15 +0200

    Added french translation. Thanks to chtiland.

---
+++ lang/fr/lang.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * additional language strings used by plugin
+ *
+ * french version
+ *
+ * @author chtiland <https://forum.dokuwiki.org/user/2611>
+ */
+$lang['S'] = 'Shift';
+$lang['A'] = 'Alt';
+$lang['C'] = 'Ctrl';
+$lang['Up'] = '&uarr;';
+$lang['Down'] = '&darr;';
+$lang['Left'] = '&larr;';
+$lang['Right'] = '&rarr;';
+$lang['PageUp'] = 'Page&uarr;';
+$lang['PageDown'] = 'Page&darr;';
+$lang['Minus'] = '&minus;';
+$lang['Ampersand'] = "&amp;";
+$lang['LessThan'] = '&lt;';
+$lang['GreaterThan'] = '&gt;';
+$lang['Windows'] = '⊞ Win'; 
+$lang['Tab'] = 'Tab ⇆'; 
+$lang['Shift'] = '⇧ Shift'; 
+$lang['Enter'] = '↵ Enter'; 
+$lang['Entrée'] = '↵ Entrée';
+
+$lang['qb_keyboard'] = 'Afficher le texte comme des touches clavier';
+
