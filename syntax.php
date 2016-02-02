ea6474289659ff57b0d350549c89442715765b01
Andreas Gohr <andi@splitbrain.org>
2016-02-02 16:08:57 +0100

    Adjust method signatures to match parent

--- syntax.php
+++ syntax.php
@@ -53,7 +53,7 @@ class syntax_plugin_keyboard extends DokuWiki_Syntax_Plugin {
     /**
      * Handle the match
      */
-    function handle($match, $state, $pos, &$handler) {
+    function handle($match, $state, $pos, Doku_Handler $handler) {
         switch ($state) {
             case DOKU_LEXER_ENTER :
                 return array($state, '');
@@ -82,7 +82,7 @@ class syntax_plugin_keyboard extends DokuWiki_Syntax_Plugin {
     /**
      * Create output
      */
-    function render($mode, &$renderer, $data) {
+    function render($mode, Doku_Renderer $renderer, $data) {
         if ($mode == 'xhtml') {
             list($state, $match) = $data;
             switch ($state) {
