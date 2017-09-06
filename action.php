088668372390b7a45f9750252382b504fd6cad33
LarsDW223 <lars_paulsen@web.de>
2017-09-06 16:19:56 +0200

    Removed tabs.

--- action.php
+++ action.php
@@ -9,41 +9,40 @@
 
 // must be run within Dokuwiki
 if (!defined('DOKU_INC'))
-	die();
+    die();
 
 if (!defined('DOKU_PLUGIN'))
-	define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
+    define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
 require_once (DOKU_PLUGIN . 'action.php');
 
 if (!defined('NL'))
-	define('NL', "\n");
+    define('NL', "\n");
 
 class action_plugin_keyboard extends DokuWiki_Action_Plugin {
 
-	/**
-	 * Register the eventhandlers
-	 */
-	function register(Doku_Event_Handler $contr) {
-		$contr->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button', array ());
-	}
-	
-	/**
-	 * Inserts the toolbar button
-	 */
-	function insert_button(&$event, $param) {
-		$event->data[] = array(	
-			'type'   => 'format',
-			'title'  => $this->getLang('qb_keyboard'),
-			'icon'   => '../../plugins/keyboard/keyboard.png',
-			'open'   => '<key>',
-			'close'  => '</key>',
-		);
-	
-		// Code for backwards compatibility
-		if (!method_exists ($event , 'mayRunDefault')) {
-			return $event->_default;
-		}
-		return $event->mayRunDefault();
-	}
+    /**
+     * Register the eventhandlers
+     */
+    function register(Doku_Event_Handler $contr) {
+        $contr->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button', array ());
+    }
 
+    /**
+     * Inserts the toolbar button
+     */
+    function insert_button(&$event, $param) {
+        $event->data[] = array(	
+            'type'   => 'format',
+            'title'  => $this->getLang('qb_keyboard'),
+            'icon'   => '../../plugins/keyboard/keyboard.png',
+            'open'   => '<key>',
+            'close'  => '</key>',
+        );
+
+        // Code for backwards compatibility
+        if (!method_exists ($event , 'mayRunDefault')) {
+            return $event->_default;
+        }
+        return $event->mayRunDefault();
+    }
 }
